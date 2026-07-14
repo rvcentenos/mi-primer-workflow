ce0d59b296337c2e41f9e9f9de24445e490d9c21
Rodolfo Centeno <rvcentenos@gmail.com>
2026-07-14 06:30:48 -0500

    Quito entorno virtual de python

---
+++ script.py
@@ -0,0 +1,7 @@
+import requests
+
+# Realiza una petición HTTP GET a una API de prueba
+respuesta = requests.get("https://www.python.org")
+
+# Muestra el código de estado (200 significa éxito)
+print(f"Estado de la petición: {respuesta.status_code}")
